9b753d86895b6bb05c7f4972cdd9893e3c297a63
Luis Cervantes <luiscerv1229@gmail.com>
2020-05-02 14:55:37 -0700

    Added Read Me boiler plate,a few comments and formatted the code to look better

--- Assets/schedule.js
+++ Assets/schedule.js
@@ -1,2 +1,92 @@
+const momentApp = moment();
 
+var toDo;
+var hourTime;
 
+// uses the moment.js functionality to show this data on the top of the calender
+
+$("#currentDay").text(momentApp.format("dddd, MMMM Do"));
+
+//calls these 2 functions onto the document itself
+$(document).ready(function () {
+  colorChange();
+  renderText();
+});
+//color change functionality for past, present, and future time slots
+function colorChange() {
+  var realTime = moment().hours();
+
+  console.log("Current Time" + realTime);
+
+  $(".input").each(function () {
+    var timeTest = parseInt($(this).attr("id"));
+    console.log(timeTest);
+
+    if (realTime > timeTest) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (realTime < timeTest) {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    } else {
+      $(this).removeClass("future");
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    }
+  });
+}
+
+// save button stores in local storage and calls the other 2 functions.
+
+$(".saveBtn").click(function () {
+  toDo = $(this).siblings(".input").val();
+  console.log(toDo);
+  hourTime = $(this).siblings(".hour").text();
+  console.log(hourTime);
+  localStorage.setItem(hourTime, JSON.stringify(toDo));
+
+  colorChange();
+  renderText();
+});
+
+//saves the actual words typed in the specific time slot
+
+function renderText() {
+  var saveWords8 = JSON.parse(localStorage.getItem("8:00 am"));
+  $("#8").val("");
+  $("#8").val(saveWords8);
+
+  var saveWords9 = JSON.parse(localStorage.getItem("9:00 am"));
+  $("#9").val("");
+  $("#9").val(saveWords9);
+
+  var saveWords10 = JSON.parse(localStorage.getItem("10:00 am"));
+  $("#10").val("");
+  $("#10").val(saveWords10);
+
+  var saveWords11 = JSON.parse(localStorage.getItem("11:00 am"));
+  $("#11").val("");
+  $("#11").val(saveWords11);
+
+  var saveWords12 = JSON.parse(localStorage.getItem("12:00 pm"));
+  $("#12").val("");
+  $("#12").val(saveWords12);
+
+  var saveWords1 = JSON.parse(localStorage.getItem("1:00 pm"));
+  $("#13").val("");
+  $("#13").val(saveWords1);
+
+  var saveWords2 = JSON.parse(localStorage.getItem("2:00 pm"));
+  $("#14").val("");
+  $("#14").val(saveWords2);
+
+  var saveWords3 = JSON.parse(localStorage.getItem("3:00 pm"));
+  $("#15").val("");
+  $("#15").val(saveWords3);
+
+  var saveWords4 = JSON.parse(localStorage.getItem("4:00 pm"));
+  $("#16").val("");
+  $("#16").val(saveWords4);
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Scheduler
+
+An Awesome daily scheduler
+
+## Table of Contents
+
+- [Name](#Scheduler)
+- [Description](#Description)
+- [instructions](instructions)
+- [Support](#support)
+- [Contributing](#contributing)
+
+## Description
+
+A simple calendar application that allows the user to save events for each hour of the day (This one allows from 8:00am - 4:00pm).
+If an event is in the past, the background will turn gray, if an event is in the present, the background will turn light red , and if an event is in the past, the background will turn green.
+
+## instructions
+
+Select a text area and type in what ever activity you want to do at that time. Click the save icon and the acivity will remain in that time slot.
+
+## Support
+
+Please [open an issue](https://github.com/fraction/readme-boilerplate/issues/new) for support.
+
+## Contributing
+
+Please contribute using [Github Flow](https://guides.github.com/introduction/flow/). Create a branch, add commits, and [open a pull request](https://github.com/fraction/readme-boilerplate/compare/).
